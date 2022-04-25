ad12d022b8c19d1435c7de9572effc4e672ced84
Philip Riecks <mail@philipriecks.de>
2022-04-25 07:15:47 +0200

    add empty project skeleton

---
+++ fixing-a-broken-window/src/test/java/de/rieckpil/talks/FixingABrokenWindowApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rieckpil.talks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixingABrokenWindowApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
