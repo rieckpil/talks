7657edc08350606932f48e0fbaa1e5fd4f98b7c5
Philip Riecks <mail@philipriecks.de>
2022-05-06 06:50:31 +0200

    adjust codebase

--- fixing-a-broken-window/src/test/java/de/rieckpil/talks/ApplicationIT.java
+++ fixing-a-broken-window/src/test/java/de/rieckpil/talks/ApplicationIT.java
@@ -1,8 +1,10 @@
 package de.rieckpil.talks;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -23,11 +25,20 @@ class ApplicationIT {
   @Autowired
   private WebTestClient webTestClient;
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
   @DynamicPropertySource
   static void postgresqlProperties(DynamicPropertyRegistry registry) {
     registry.add("spring.datasource.url", database::getJdbcUrl);
     registry.add("spring.datasource.password", database::getPassword);
     registry.add("spring.datasource.username", database::getUsername);
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println(applicationContext.getClass());
+    System.out.println(applicationContext.getBeanDefinitionCount());
   }
 
   @Test
