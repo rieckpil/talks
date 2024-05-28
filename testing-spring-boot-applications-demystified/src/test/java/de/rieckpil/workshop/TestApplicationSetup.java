0295251dbcd4ee0e911264d5135fe12554443508
Philip Riecks <mail@philipriecks.de>
2024-05-28 10:24:27 +0200

    adjust sample•

--- testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/workshop/TestApplicationSetup.java
+++ testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/workshop/TestApplicationSetup.java
@@ -8,16 +8,16 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestTestingSpringBootApplicationsDemystifiedApplication {
+public class TestApplicationSetup {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-	}
+  public static void main(String[] args) {
+    SpringApplication.from(Application::main).with(TestApplicationSetup.class).run(args);
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.from(TestingSpringBootApplicationsDemystifiedApplication::main).with(TestTestingSpringBootApplicationsDemystifiedApplication.class).run(args);
-	}
+  @Bean
+  @ServiceConnection
+  PostgreSQLContainer<?> postgresContainer() {
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+  }
 
 }
