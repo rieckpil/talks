65f27ffac6ac89ba309e94e7624d797193a1efb2
Philip Riecks <mail@philipriecks.de>
2024-05-30 13:26:37 +0200

    context

--- testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/workshop/advanced/ContextReuseTwoIT.java
+++ testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/workshop/advanced/ContextReuseTwoIT.java
@@ -3,6 +3,7 @@ package de.rieckpil.workshop.advanced;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.cache.ContextCacheUtils;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -11,5 +12,13 @@ public class ContextReuseTwoIT {
   @Test
   void doStuff() {
     System.out.println("Doing stuff in second test");
+  }
+
+  @Test
+  void configureCache() {
+    // spring.test.context.cache.maxSize=42
+    System.setProperty("spring.test.context.cache.maxSize", "13");
+    System.out.println(ContextCacheUtils.retrieveMaxCacheSize());
+    System.out.println(ContextCacheUtils.retrieveContextFailureThreshold());
   }
 }
