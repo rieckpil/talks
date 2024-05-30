bb51514656bba70afb02850a1dd4190ce77df0dd
Philip Riecks <mail@philipriecks.de>
2024-05-30 07:29:01 +0200

    code

---
+++ testing-spring-boot-applications-demystified/src/main/java/de/rieckpil/workshop/CustomerController.java
@@ -0,0 +1,35 @@
+package de.rieckpil.workshop;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+  private final CustomerService customerService;
+
+  public CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> createNewCustomer(
+    @RequestBody ObjectNode payload,
+    UriComponentsBuilder uriComponentsBuilder) {
+
+    Long customerId = customerService.createNewCustomer(payload);
+
+    UriComponents uriComponents = uriComponentsBuilder.path("/api/customers/{id}")
+      .buildAndExpand(customerId);
+
+    return ResponseEntity.created(uriComponents.toUri()).build();
+  }
+}
+
---
+++ testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/workshop/basics/CustomerControllerTest.java
@@ -0,0 +1,55 @@
+package de.rieckpil.workshop.basics;
+
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import de.rieckpil.workshop.CustomerController;
+import de.rieckpil.workshop.CustomerService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private CustomerService customerService;
+
+  @Test
+  void shouldReturnLocationOfNewlyCreatedCustomer() throws Exception {
+
+    when(customerService.createNewCustomer(any(ObjectNode.class)))
+      .thenReturn(42L);
+
+    this.mockMvc
+      .perform(post("/api/customers")
+        .contentType(APPLICATION_JSON)
+        .content("""
+           {
+             "first_name": "John",
+             "last_name": "Doe",
+             "email": "john.doe@devoxx.be"
+           }
+          """)
+      )
+      .andExpect(status().isCreated())
+
+      .andExpect(header().string("Location",
+        Matchers.containsString("/api/customers/42")));
+  }
+}
+
+
+
