ddb64032d6dd049b4f670c723a5c1c6d5b8cb65f
Philip Riecks <mail@philipriecks.de>
2025-05-13 15:14:34 +0200

    wip

--- testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/talks/basics/CustomerControllerTest.java
+++ testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/talks/basics/CustomerControllerTest.java
@@ -8,7 +8,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -24,7 +24,7 @@ class CustomerControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
+  @MockitoBean
   private CustomerService customerService;
 
   @Test
