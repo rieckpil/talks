35ade18d5d274bf0d2cc4063e28d60c2b750ae98
Philip Riecks <mail@philipriecks.de>
2025-05-15 10:40:28 +0200

    remove jwt

--- testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/talks/basics/CustomerControllerTest.java
+++ testing-spring-boot-applications-demystified/src/test/java/de/rieckpil/talks/basics/CustomerControllerTest.java
@@ -8,14 +8,12 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,7 +28,6 @@ class CustomerControllerTest {
   private CustomerService customerService;
 
   @Test
-  @WithMockUser
   void shouldReturnLocationOfNewlyCreatedCustomer() throws Exception {
 
     when(customerService.createNewCustomer(any(ObjectNode.class)))
@@ -38,7 +35,6 @@ class CustomerControllerTest {
 
     this.mockMvc
       .perform(post("/api/customers")
-        .with(jwt())
         .contentType(APPLICATION_JSON)
         .content("""
            {
